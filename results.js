4d40c39bb0454e908d0df332597a8aaa3a6d7a92
Whelan Boyd <whelanboyd@gmail.com>
2014-09-23 14:00:01 -0700

    Added launchWinner method

--- results.js
+++ results.js
@@ -2,10 +2,10 @@
 function optimizelyResultsPage() {
 	var $ = jQuery;
 
-
 	var projectId = $('#optimizely_project_id').val();  
 	var optly = new OptimizelyAPI($("#optimizely_token").val());  
 	
+	//fetch only Wordpress experiments from project
 	optly.get('projects/' + projectId + '/experiments/', function(response) {
 		optly.wordpressExps = [];
 		for (i=0; i<response.length; i++) {
@@ -16,24 +16,31 @@ function optimizelyResultsPage() {
 		getWPExpResults(optly.wordpressExps);
 	});
 
+	//launch winning variation when Launch button is clicked...will this work with 
+	$(".launch").bind('click', function() {
+   		var loserVars = [];
+   		$(this).parents('.opt_results').find("tr:not('.winner')").each(function() {  //[NEED] loser class or use "tr:not('.winner')""
+      		loserVars.push($(this).attr('data-var-id'));
+   			launchWinner(loserVars);
+		});
+	});
+
 	function getWPExpResults(WPexpArray) {
 		optly.results = [];
-		for (j=0; j<expArray.length; j++){
-			optly.get('experiments/' + WPexpArray[j].id + '/results', function(response) { //this is throwing an error
-				 
+		for (j=0; j < WPexpArray.length; j++){
+			optly.get('experiments/' + WPexpArray[j].id + '/results', function(response) { 
 				 optly.results.push(response);
 			});
 		}
 	}
 
-	/* function launchVariation(experiment) {
-		var varId = $(".variation_id[data=XXXXXXXX]").val();  //store var id
-    	$('#optimizely_launch_var').text('Launching...');  //[NEED] update selectors, etc.
-    	//for each variation that is not the one being launched, set is_paused=true
-    	optly.patch('variations/' + optly.experiment.results[The other vars], {'is_paused': 'true'}, function(response) {
-      		optly.experiment = response;
-  			//need to call function that updates UI
-    	});
-  	} */
+	function launchWinner(loserArray) {
+		for (i=0; i<loserArray.length; i++) {
+			optly.patch('variations/' + loserArray[i], {'is_paused': 'true'}, function(response) {
+      			optly.experiment = response;
+      			//function to update UI
+    		});
+		}	
+  	} 
 
 }
