3803ad8dcd950e41a021aa871a7fa3efd4dd86c2
Brad Taylor <brad@optimizely.com>
2014-09-25 12:02:59 -0700

    added progress bar and powered testing

--- results.js
+++ results.js
@@ -1,65 +1,62 @@
 
-function optimizelyResultsPage(apiToken,projectId) {
-	var $ = jQuery;
+function optimizelyResultsPage(apiToken,projectId,poweredVisitor) {
+	 var $ = jQuery;
 
-	var optly = new OptimizelyAPI(apiToken);  
+	 var optly = new OptimizelyAPI(apiToken);  
 
-	//fetch only Wordpress experiments from project
-	optly.get('projects/' + projectId + '/experiments/', function(response) {
-		optly.wordpressExps = [];
-		for (i=0; i<response.length; i++) {
-			// response[i].description.indexOf('Wordpress') > -1 && 
-			if (response[i].status != 'Archived' && response[i].status != 'Draft') {
-				//optly.wordpressExps.push(response[i]);
-				getWPExpResults(response[i],function(exp){
-					displayResultsList(exp,i);
-				});
-			}
-		}
-	});
+  	//fetch only Wordpress experiments from project
+  	optly.get('projects/' + projectId + '/experiments/', function(response) {
+  		optly.wordpressExps = [];
+  		for (i=0; i<response.length; i++) {
+  			if (response[i].description.indexOf('Wordpress') > -1 && response[i].status != 'Archived' && response[i].status != 'Draft') {
+  				getWPExpResults(response[i],function(exp){
+  					displayResultsList(exp,i);
+  				});
+  			}
+  		}
+  	});
 
-	//launch winning variation when Launch button is clicked...will this work with 
-	$("html").delegate(".launch", 'click', function() {
-			var loserVars = [];
-			$(this).parents('.opt_results').find("tr:not('.winner')").each(function() {  //[NEED] loser class or use "tr:not('.winner')""
-  			loserVars.push($(this).attr('data-var-id'));
-				launchWinner(loserVars);
-		});
-	});
+  	//launch winning variation when Launch button is clicked...will this work with 
+  	$("html").delegate(".launch", 'click', function() {
+  			var loserVars = [];
+  			$(this).parents('.opt_results').find("tr:not('.winner')").each(function() {  //[NEED] loser class or use "tr:not('.winner')""
+    			loserVars.push($(this).attr('data-var-id'));
+  				launchWinner(loserVars);
+  		});
+  	});
 
-	//pause experiment when pause button is pressed
-	$("html").delegate(".pause", 'click', function() {
-		var expID = $(this).parents('.opt_results').attr("data-exp-id");
-		pauseExperiment(expID);
-	});
-	//start experiment when play button is pressed
-	$("html").delegate(".play", 'click', function() {
-		console.log("clicked play");
-		var expID = $(this).parents('.opt_results').attr("data-exp-id");
-		startExperiment(expID);
-	});
+  	//pause experiment when pause button is pressed
+  	$("html").delegate(".pause", 'click', function() {
+  		var expID = $(this).parents('.opt_results').attr("data-exp-id");
+  		pauseExperiment(expID);
+  	});
+  	//start experiment when play button is pressed
+  	$("html").delegate(".play", 'click', function() {
+  		console.log("clicked play");
+  		var expID = $(this).parents('.opt_results').attr("data-exp-id");
+  		startExperiment(expID);
+  	});
 
-	function getWPExpResults(expObj,cb) {
-		expObj.results = [];
-		optly.get('experiments/' + expObj.id + '/results', function(response) { 
-			for(i=0;i < response.length;i++){
-				var result = response[i];
-				//if(result.goal_name.indexOf('Views to page') > -1){
-					expObj.results.push(result);
-				//}
-			}
-			//finalExpArray.push(expObj);
-			cb(expObj);
-		});
-	}
+  	function getWPExpResults(expObj,cb) {
+  		expObj.results = [];
+  		optly.get('experiments/' + expObj.id + '/results', function(response) { 
+  			for(i=0;i < response.length;i++){
+  				var result = response[i];
+  				if(result.goal_name.indexOf('Views to page') > -1){
+  					expObj.results.push(result);
+  				}
+  			}
+  			cb(expObj);
+  		});
+  	}
 
-	function launchWinner(loserArray) {
-		for (i=0; i<loserArray.length; i++) {
-			optly.patch('variations/' + loserArray[i], {'is_paused': 'true'}, function(response) {
-      			optly.experiment = response;
-      			//function to update UI
-    		});
-		}	
+  	function launchWinner(loserArray) {
+  		for (i=0; i<loserArray.length; i++) {
+  			optly.patch('variations/' + loserArray[i], {'is_paused': 'true'}, function(response) {
+        			optly.experiment = response;
+        			//function to update UI
+      		});
+  		}	
   	}
 
   	function pauseExperiment(experiment) {
@@ -81,7 +78,57 @@ function optimizelyResultsPage(apiToken,projectId) {
   	function displayResultsList(exp,i) {
   		$('.loading').hide();
   		$('#results_list').append(buildResultsModuleHTML(exp));
+      animateProgressBar(exp);
+      
   	}
+
+    function getAverageVisitor(results){
+      debugger;
+      var totalVisitors = 0;
+      for(var i=0;i < results.length;i++){
+        totalVisitors += results[i].visitors;
+      }
+
+      return totalVisitors/results.length;
+    }
+
+    function animateProgressBar(exp){
+      var progressbar = $('#exp_'+exp.id).find('.progressbar');
+      //var averageVisitorPerVariation = getAverageVisitor(exp.results);
+      var averageVisitorPerVariation = 20000,
+          poweredPercentage = Math.round((averageVisitorPerVariation/poweredVisitor)*100);
+      progressbar.progressbar({
+        value: averageVisitorPerVariation,
+        max: poweredVisitor
+      });
+
+      var progBarColor = '#FF0000';
+      switch(true){
+        case (poweredPercentage < 10):
+          progBarColor = "#FF0011";
+          break;
+        case (poweredPercentage >= 10 && poweredPercentage < 30):
+          progBarColor = "#FF0011";
+          break;
+        case (poweredPercentage >= 30 && poweredPercentage < 50):
+          progBarColor = "#FF0011";
+          break;
+        case (poweredPercentage >= 50 && poweredPercentage < 70):
+          progBarColor = "#FF0011";
+          break;
+        case (poweredPercentage >= 70 && poweredPercentage < 90):
+          progBarColor = "#FF0011";
+          break;
+        case (poweredPercentage >= 90 && poweredPercentage < 100):
+          progBarColor = "#FF0011";
+          break;
+        default:
+          progBarColor = "#00FF00";
+          break;
+      }
+
+      $(progressbar).find('.ui-progressbar-value').css({'background':progBarColor,'border':'1px solid '+progBarColor});
+    }
 
 
   	function buildResultsModuleHTML(exp) {
@@ -91,51 +138,54 @@ function optimizelyResultsPage(apiToken,projectId) {
   			statusClass = 'pause';
   		}
 
-		var html = ""+
-		'<div id="exp_'+exp.id+'" data-exp-id="'+exp.id+'" class="opt_results">'+
-            '<div class="header">'+
-                '<div class="title"><i class="fa fa-check-circle fa-fw"></i>'+exp.description+'</div>'+
-                '<div class="results_toolbar">'+
-                    '<div class="'+statusClass+' button">'+
-                        '<i class="fa fa-'+statusClass+' fa-fw"></i>'+
-                    '</div>'+
-                    '<div class="edit button">'+
-                        '<i class="fa fa-edit fa-fw"></i>'+
-                    '</div>'+
-                    '<div class="prev button">'+
-                        '<i class="fa fa-eye fa-fw"></i>'+
-                    '</div>'+
-                    '<div class="launch button">'+
-                        '<i class="fa fa-rocket fa-fw"></i>'+
-                    '</div>'+
-                '</div>'+
-            '</div>'+
-            '<div class="variations">'+
-                '<table>'+
-                    '<tr class="first">'+
-                        '<th class="first">VARIATION</th>'+
-                        '<th>VISITORS</th>'+
-                        '<th>CONVERSIONS</th>'+
-                        '<th>CONVERSION RATE</th>'+
-                        '<th>IMPROVEMENT</th>'+
+	    var html = ""+
+	    '<div id="exp_'+exp.id+'" data-exp-id="'+exp.id+'" class="opt_results">'+
+          '<div class="header">'+
+              '<div class="title">'+exp.description+'</div>'+
+              '<div class="results_toolbar">'+
+                  '<div class="'+statusClass+' button">'+
+                      '<i class="fa fa-'+statusClass+' fa-fw"></i>'+
+                  '</div>'+
+                  '<div class="edit button">'+
+                      '<i class="fa fa-edit fa-fw"></i>'+
+                  '</div>'+
+                  '<div class="prev button">'+
+                      '<i class="fa fa-eye fa-fw"></i>'+
+                  '</div>'+
+                  '<div class="launch button">'+
+                      '<i class="fa fa-rocket fa-fw"></i>'+
+                  '</div>'+
+              '</div>'+
+          '</div>'+
+          '<div class="variations">'+
+              '<table>'+
+                  '<tr class="first">'+
+                      '<th class="first">VARIATION</th>'+
+                      '<th>VISITORS</th>'+
+                      '<th>CONVERSIONS</th>'+
+                      '<th>CONVERSION RATE</th>'+
+                      '<th>IMPROVEMENT</th>'+
+                  '</tr>';
+                  for(i=0;i < exp.results.length;i++){
+                  	var result = exp.results[i];
+                  	html = html+
+                  	'<tr class="'+result.status+'" data-var-id="">'+
+                        '<td class="first">'+result.variation_name+'</td>'+
+                        '<td>'+result.visitors+'</td>'+
+                        '<td>'+result.conversions+'</td>'+
+                        '<td>'+result.conversion_rate+'</td>'+
+                        '<td>'+result.improvement+'</td>'+
                     '</tr>';
-                    for(i=0;i < exp.results.length;i++){
-                    	var result = exp.results[i];
-                    	html = html+
-                    	'<tr class="'+result.status+'" data-var-id="">'+
-	                        '<td class="first">'+result.variation_name+'</td>'+
-	                        '<td>'+result.visitors+'</td>'+
-	                        '<td>'+result.conversions+'</td>'+
-	                        '<td>'+result.conversion_rate+'</td>'+
-	                        '<td>'+result.improvement+'</td>'+
-	                    '</tr>';
-                    }
-                    
-                    html = html+
-                '</table>'+
-            '</div>'+
-        '</div>';
-        return html;
+                  }
+                  
+                  html = html+
+              '</table>'+
+          '</div>'+
+          '<div class="footer">'+
+              '<div class="progressbar"></div><i class="fa fa-clock-o fa-fw"></i> Not Ready Yet!'+
+          '</div>'+
+      '</div>';
+      return html;
 	}
 
 }
